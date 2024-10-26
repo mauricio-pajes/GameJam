1d1d8f759218a9d66b9ccb7964ffadd2b2c4b208
mauricio-pajes <mpajes2006@gmail.com>
2024-10-26 00:46:41 -0500

    Menu

--- GameJam/MyForm.h
+++ GameJam/MyForm.h
@@ -2,64 +2,89 @@
 
 namespace GameJam {
 
-	using namespace System;
-	using namespace System::ComponentModel;
-	using namespace System::Collections;
-	using namespace System::Windows::Forms;
-	using namespace System::Data;
-	using namespace System::Drawing;
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
 
-	/// <summary>
-	/// Summary for MyForm
-	/// </summary>
-	public ref class MyForm : public System::Windows::Forms::Form
-	{
-	public:
-		MyForm(void)
-		{
-			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
-		}
+    public ref class MyForm : public System::Windows::Forms::Form
+    {
+    public:
+        MyForm(void)
+        {
+            InitializeComponent();
+        }
 
-	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		~MyForm()
-		{
-			if (components)
-			{
-				delete components;
-			}
-		}
+    protected:
+        ~MyForm()
+        {
+            if (components)
+            {
+                delete components;
+            }
+        }
 
-	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		System::ComponentModel::Container ^components;
+    private:
+        System::ComponentModel::Container^ components;
+        System::Windows::Forms::Button^ botonCarrera;
+        System::Windows::Forms::Button^ botonGenerarCarritos;
+        System::Windows::Forms::Label^ etiquetaMenu;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		void InitializeComponent(void)
-		{
-			this->SuspendLayout();
-			// 
-			// MyForm
-			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(566, 339);
-			this->Name = L"MyForm";
-			this->Text = L"MyForm";
-			this->ResumeLayout(false);
+        void InitializeComponent(void)
+        {
+            this->botonCarrera = (gcnew System::Windows::Forms::Button());
+            this->botonGenerarCarritos = (gcnew System::Windows::Forms::Button());
+            this->etiquetaMenu = (gcnew System::Windows::Forms::Label());
+            this->SuspendLayout();
 
-		}
+            // 
+            // etiquetaMenu
+            // 
+            this->etiquetaMenu->AutoSize = true;
+            this->etiquetaMenu->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24));
+            this->etiquetaMenu->Location = System::Drawing::Point(200, 50);
+            this->etiquetaMenu->Name = L"etiquetaMenu";
+            this->etiquetaMenu->Size = System::Drawing::Size(100, 37);
+            this->etiquetaMenu->TabIndex = 2;
+            this->etiquetaMenu->Text = L"Menú";
+
+            // 
+            // botonCarrera
+            // 
+            this->botonCarrera->Location = System::Drawing::Point(80, 150);
+            this->botonCarrera->Name = L"botonCarrera";
+            this->botonCarrera->Size = System::Drawing::Size(150, 50);
+            this->botonCarrera->TabIndex = 0;
+            this->botonCarrera->Text = L"Carrera :D";
+            this->botonCarrera->UseVisualStyleBackColor = true;
+
+            // 
+            // botonGenerarCarritos
+            // 
+            this->botonGenerarCarritos->Location = System::Drawing::Point(280, 150);
+            this->botonGenerarCarritos->Name = L"botonGenerarCarritos";
+            this->botonGenerarCarritos->Size = System::Drawing::Size(150, 50);
+            this->botonGenerarCarritos->TabIndex = 1;
+            this->botonGenerarCarritos->Text = L"Generación Automática de Carritos";
+            this->botonGenerarCarritos->UseVisualStyleBackColor = true;
+
+            // 
+            // MyForm
+            // 
+            this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+            this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+            this->ClientSize = System::Drawing::Size(515, 365);
+            this->Controls->Add(this->etiquetaMenu);
+            this->Controls->Add(this->botonCarrera);
+            this->Controls->Add(this->botonGenerarCarritos);
+            this->Name = L"MyForm";
+            this->Text = L"MyForm";
+            this->ResumeLayout(false);
+            this->PerformLayout();
+        }
 #pragma endregion
-	};
+    };
 }
